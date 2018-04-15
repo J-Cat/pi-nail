4b41fcde101efeb8e3bf3026a2a216be90b18e8f
Joe Cataford <jcataford@linamar.com>
2018-04-14 22:55:29 -0400

    Added readme.md and did some cleanup such as moving blessed-contrib copy back to fork of original github source, updating package.json accordingly.

--- blessed-contrib/examples/grid.js
+++
@@ -1,9 +0,0 @@
-
-var blessed = require('blessed')
-  , contrib = require('../')
-  , screen = blessed.screen()
-  , grid = new contrib.grid({rows: 12, cols: 12, screen: screen})
-  , map = grid.set(0, 0, 4, 4, contrib.map, {label: 'World Map'})
-  , box = grid.set(4, 4, 4, 4, blessed.box, {content: 'My Box'})
-
-screen.render()
--- blessed-contrib/examples/inline-data/gauge.js
+++
@@ -1,7 +0,0 @@
-var blessed = require('blessed')
-  , contrib = require('../../')
-  , screen = blessed.screen()
-  , gauge = contrib.gauge({label: 'Progress', percent: 25})
-    
-screen.append(gauge)
-screen.render()
